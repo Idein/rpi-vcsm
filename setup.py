1c19238eebcd51bd99cc16f29f000eb27e375b55
Sugizaki Yukimasa <ysugi@idein.jp>
2018-01-07 00:02:11 +0900

    Add installation files

---
+++ setup.py
@@ -0,0 +1,17 @@
+from distutils.core import setup
+from Cython.Build import cythonize
+
+# Note:
+#   - For packaging, see http://www.diveintopython3.net/packaging.html
+#   - Calculate version by using https://www.pakin.org/~scott/ltver.html
+
+setup(
+        name = "rpi-vcsm",
+        packages = ["rpi_vcsm"],
+        version = "0.0.0",
+        description = "VideoCore Shared Memory (VCSM) driver for Raspberry Pi",
+        author = "Sugizaki Yukimasa",
+        author_email = "ysugi@idein.inc",
+        url = "https://github.com/Idein/rpi-vcsm",
+        ext_modules = cythonize("rpi_vcsm/buffer.pyx"),
+)
